81c28e67c0e4c3b00d3221d75f5c2a5ba0251b2e
Antonio <deitonfree@gmail.com>
2022-09-16 17:27:25 -0500

    Post Activity

--- app/src/main/java/com/thisname/coffee/activitys/fragments/HomeFragment.java
+++ app/src/main/java/com/thisname/coffee/activitys/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.thisname.coffee.activitys.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.thisname.coffee.R;
+import com.thisname.coffee.activitys.PostActivity;
+import com.thisname.coffee.activitys.RegisterActivity;
 
 
 public class HomeFragment extends Fragment {
 
+
+    View mView;
+    FloatingActionButton mFab;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,9 +27,22 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_home,container, false);
+        mFab = mView.findViewById(R.id.fab);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+
+        return mView;
+    }
+
+    private void  goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
     }
 }
